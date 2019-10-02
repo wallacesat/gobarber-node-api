10e1437bbd0c9d8f62d477d4a03e829187ef8860
Wallace Saturnino <wallace@MacBook-Pro-de-Wallace.local>
2019-10-02 16:24:13 -0300

    Criando o loader de Models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Wallace' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego',
+    email: 'diego@bla.com',
+    password_hash: '123123',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
