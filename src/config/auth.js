de94b056d53d05200796004ae9f5b24741f2746d
Wallace Saturnino <wallace@MacBook-Pro-de-Wallace.local>
2019-10-08 14:38:58 -0300

    Configurando variáveis ambiente

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '2555a367f26a45428eb57d6b72ecc240',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,4 @@
 export default {
-  dsn: 'https://25dec025cea6486d9e215dfc1d392e24@sentry.io/1773468',
+  // dsn: 'https://25dec025cea6486d9e215dfc1d392e24@sentry.io/1773468',
+  dsn: process.env.SENTRY_DSN,
 };
