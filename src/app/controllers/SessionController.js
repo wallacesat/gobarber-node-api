250ac448b2b5eb6cf643eca7c3108b8e1a069f08
Wallace Saturnino <wallace@MacBook-Pro-de-Wallace.local>
2019-10-14 15:23:11 -0300

    Ajustes no backend

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,7 +2,9 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
+
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -23,6 +25,13 @@ class SessionController {
 
     const user = await User.findOne({
       where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
     });
 
     if (!user) {
@@ -37,13 +46,15 @@ class SessionController {
       });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
